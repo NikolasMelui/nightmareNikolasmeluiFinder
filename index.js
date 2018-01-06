f934bf7d8f0d5dae202b0a88f4f2ffe7be4a66af
NikolasMelui <slak@samaradom.ru>
2018-01-07 03:28:41 +0400

    Add evaluate function

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const Nightmare = require('nightmare');
-const dream = Nightmare({ show: 'true' });
 
+const dream = Nightmare({ show: 'true' });
 dream
 	.goto('https://duckduckgo.com')
 	.type('input', 'NikolasMelui') // First arg is field, second is info
 	.click('input#search_button_homepage')
 	.wait(3000)
+	.evaluate(() => document.querySelector('div#links_wrapper').innerHTML)
 	// .end() TODO: close window
-	.then(result => {
-		console.log('Okey!');
+	.then(res => {
+		console.log(res);
 	});
