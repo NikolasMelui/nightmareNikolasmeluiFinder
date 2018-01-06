cd39eeac7fb02c2905dc8ab3d13dd6b74a2fc810
NikolasMelui <slak@samaradom.ru>
2018-01-07 03:21:13 +0400

    Add type and click fields.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const dream = Nightmare({ show: 'true' });
 
 dream
 	.goto('https://duckduckgo.com')
+	.type('input', 'NikolasMelui') // First arg is field, second is info
+	.click('input#search_button_homepage')
 	.wait(3000)
 	// .end() TODO: close window
 	.then(result => {
